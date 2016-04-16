a9007543a0862e792c9502376cb78fa1a388e919
Albert Pang <albert.pang@me.com>
2016-04-16 20:20:17 +0800

    Added state transition function to Binomial struct because both forward shooting and backward induction depends on it and don't want to pass it around Sadly, Binomial struct can't derive Debug any more because of this Added NodeValueFunc Added calc_up_prob, and backward_induce Added test case for pricing vanilla call option

--- src/lattice.rs
+++ src/lattice.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
 use std::cell::RefCell;
 
-#[derive(Debug, Clone)]
+pub type StateTransitionFunc = Box<Fn((isize, isize, isize), isize)->isize>;
+
+#[allow(dead_code)]
 pub struct Binomial {
     s0: f64,
     r: f64,
@@ -11,17 +13,27 @@ pub struct Binomial {
     dt: f64,
     period: usize,
     num_nodes: usize,
-    asset_prices: Vec<f64>,
     up: f64,
     down: f64,
+    p: f64, // up probability
+    asset_prices: Vec<f64>,
     values: Vec<RefCell<HashMap<isize, f64>>>,
+    state_transition: StateTransitionFunc,
 }
 
-pub type StateTransitionFunc = Box<Fn((isize, isize, isize), isize)->isize>;
 pub type PayoffFunc = Box<Fn(&Binomial, (isize, isize, isize))->f64>;
+pub type NodeValueFunc = Box<Fn(&Binomial, (isize, isize, isize), f64)->f64>;
 
 pub fn default_state_func() -> StateTransitionFunc {
     Box::new( |(_, _, _): (isize, isize, isize), _: isize| { 0 } )
+}
+
+pub fn default_node_value()
+-> NodeValueFunc
+{
+    Box::new( |_, (_, _, _), induced_value| {
+        induced_value
+    })
 }
 
 impl Binomial {
@@ -45,12 +57,18 @@ impl Binomial {
         }
     }
 
+    fn calc_up_prob( r: f64, q: f64, v: f64, dt: f64) -> f64 {
+        let u = (v*dt.sqrt()).exp();
+        let d = 1.0/u;
+        (((r-q)*dt).exp() - d)/(u-d)
+    }
+
     pub fn get_asset_price(&self, i: isize, j: isize) -> f64 {
         self.asset_prices[Binomial::to_index(i, j)]
     }
 
     pub fn new(s0: f64, r: f64, q: f64, v: f64, t: f64, period: usize,
-        f: &StateTransitionFunc) -> Binomial {
+        f: StateTransitionFunc) -> Binomial {
         let num_nodes = Binomial::calc_num_nodes(period);
         let dt = t/period as f64;
         let u = v * dt.sqrt();
@@ -60,11 +78,13 @@ impl Binomial {
             Binomial {
                 s0: s0, r: r, q: q, v: v, t: t, period: period,
                 num_nodes: num_nodes,
-                asset_prices: Vec::with_capacity(num_nodes),
                 dt: dt, up: up, down: down,
+                p: Binomial::calc_up_prob(r, q, v, dt),
+                asset_prices: Vec::with_capacity(num_nodes),
                 values: Vec::with_capacity(num_nodes),
+                state_transition: f,
             };
-        grid.build( f );
+        grid.build();
         grid
     }
 
@@ -72,8 +92,9 @@ impl Binomial {
         self.values[Binomial::to_index(i, j)].borrow_mut().insert(k, v);
     }
 
-    fn forward_states(&mut self, (i, j):(isize, isize), f: &StateTransitionFunc)
+    fn forward_states(&mut self, (i, j):(isize, isize))
     {
+        let f = &self.state_transition;
         let current_index = Binomial::to_index(i, j);
         // i is the time index, always larger than 0, okay to cast to usize
         let down_index = current_index + i as usize + 1;
@@ -86,7 +107,7 @@ impl Binomial {
         }
     }
 
-    fn build(&mut self, f: &StateTransitionFunc) {
+    fn build(&mut self) {
         let n = self.period as isize;
         let mut v: Vec<f64> = Vec::with_capacity(2*self.period+1);
         for i in (-n)..(n+1) {
@@ -103,7 +124,7 @@ impl Binomial {
 
         // run forward-shooting
         Binomial::iter_nodes(0, n-1, &mut |(i, j)| {
-            self.forward_states((i, j), f);
+            self.forward_states((i, j));
         });
     }
 
@@ -118,10 +139,39 @@ impl Binomial {
         });
     }
 
+    fn backward_induce(&mut self, node_value: &NodeValueFunc) {
+        let n = self.period as isize;
+        let state = &self.state_transition;
+        let discount = (-self.r*self.dt).exp();
+        for i in (0..n).rev() {
+            // use index as start and inc by 1 to save calling to_index, since
+            // we know all nodes in the same period are consecutive
+            let mut index = (i * (i+1) / 2) as usize;
+            for ii in 0..(i+1) {
+                let j = -i + ii * 2;
+                for (&k, v) in self.values[index].borrow_mut().iter_mut() {
+                    let down_index = index + i as usize + 1;
+                    let up_index = down_index + 1;
+                    let up_state = state((i, j, k), j+1);
+                    let up_value = self.values[up_index].borrow()[&up_state];
+                    let down_state = state((i, j, k), j-1);
+                    let down_value =
+                        self.values[down_index].borrow()[&down_state];
+                    let induced_value = discount * (
+                        self.p*up_value+(1.0 - self.p)*down_value);
+                    *v = node_value(&self, (i, j, k), induced_value);
+                }
+                index += 1;
+            }
+        }
+    }
+
     #[allow(dead_code)]
-    fn price(&mut self, payoff: &PayoffFunc) -> f64 {
+    fn price(&mut self, payoff: &PayoffFunc, node_value: &NodeValueFunc) -> f64
+    {
         self.calc_terminal(payoff);
-        0.0
+        self.backward_induce(node_value);
+        self.values[0].borrow()[&0]
     }
 }
 
@@ -131,6 +181,8 @@ mod test {
     use lattice::*;
     use std::cmp;
     use std::collections::HashMap;
+    use option::OptionType::*;
+    use option::black_scholes;
 
     #[test]
     pub fn test_binomial_populate_and_get_asset_price() {
@@ -145,7 +197,7 @@ mod test {
         let v = 0.3;
         let t = 2.0;
         let period = 5;
-        let grid = Binomial::new(s0, r, q, v, t, period, &default_state_func());
+        let grid = Binomial::new(s0, r, q, v, t, period, default_state_func());
 
         let n = grid.period as isize;
         for i in 0..(n+1) {
@@ -173,7 +225,7 @@ mod test {
 
         let now = time::precise_time_s();
         let mut _grid = Binomial::new(s0, r, q, v, t, period,
-            &default_state_func());
+            default_state_func());
         println!("Creation time:  {}", time::precise_time_s() - now);
 
         assert!( false );
@@ -193,7 +245,7 @@ mod test {
                 cmp::max(to_j, k)
         } );
 
-        let grid = Binomial::new(s0, r, q, v, t, period, &lookback);
+        let grid = Binomial::new(s0, r, q, v, t, period, lookback);
         let (_, last_period_nodes) =
             grid.values.split_at(grid.num_nodes-grid.period-1);
 
@@ -230,7 +282,7 @@ mod test {
         let period = 4;
 
         let mut grid =
-            Binomial::new(s0, r, q, v, t, period, &default_state_func());
+            Binomial::new(s0, r, q, v, t, period, default_state_func());
         let up = grid.up;
 
         let european_payoff: PayoffFunc = Box::new(
@@ -240,7 +292,7 @@ mod test {
             }
         );
 
-        grid.price(&european_payoff);
+        grid.price(&european_payoff, &default_node_value());
 
         let (_, last_period_nodes) =
             grid.values.split_at(grid.num_nodes-grid.period-1);
@@ -258,5 +310,32 @@ mod test {
             }).collect();
 
         assert_eq!(results.as_slice(), expected.as_slice());
+    }
+
+    #[test]
+    pub fn test_vanilla_call() {
+        let s0 = 50.0;
+        let r = 0.05;
+        let q = 0.0;
+        let v = 0.3;
+        let t = 0.25;
+        let strike = 50.0;
+        let period = 1000;
+
+        let european_payoff: PayoffFunc = Box::new(
+            move |grid, (i, j, _)| {
+                let underlying_price = grid.get_asset_price(i, j);
+                (underlying_price - strike).max(0.0)
+            }
+        );
+        let now = time::precise_time_s();
+        let mut grid =
+            Binomial::new(s0, r, q, v, t, period, default_state_func());
+
+        let price = grid.price(&european_payoff, &default_node_value());
+        println!("Time taken: {}", time::precise_time_s() - now);
+
+        let expected = black_scholes(s0, r, q, v, t, Call, strike);
+        assert!( (price-expected).abs() < 0.01 );
     }
 }
